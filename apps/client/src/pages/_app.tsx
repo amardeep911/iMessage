0002e798b9081f8bdcd66a45a7f56c46ff0207d1
Amardeep Ranjan <amardeepranjan911@gmail.com>
2023-07-11 14:45:23 +0530

    configured apollo client

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,4 +1,6 @@
+import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { client } from '@src/graphql/apollo-client';
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 import { theme } from '../chakra/theme';
@@ -8,12 +10,14 @@ const MyApp = ({
   pageProps: { session, ...pageProps },
 }: AppProps) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <SessionProvider session={session}>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <Component {...pageProps} />
-    </ChakraProvider>
-  </SessionProvider>
+  <ApolloProvider client={client}>
+    <SessionProvider session={session}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
+  </ApolloProvider>
 );
 
 export default MyApp;
