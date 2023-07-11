526e30ed72be2585b4c5b9072fe9c28e609f8fe7
Amardeep Ranjan <amardeepranjan911@gmail.com>
2023-07-11 12:26:12 +0530

    fix-prisma schema auth error fixed

--- apps/client/src/pages/api/auth/[...nextauth].ts
+++ apps/client/src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,13 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 import NextAuth from 'next-auth';
-import Google from 'next-auth/providers/google';
+import GoogleProvider from 'next-auth/providers/google';
+
 const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
-    Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,9 +1,10 @@
+import { NextPage } from 'next';
 import { signIn, useSession } from 'next-auth/react';
 
-const Home = () => {
+const Home: NextPage = () => {
   const { data } = useSession();
   console.log(data);
-  console.log(process.env.GOOGLE_CLIENT_ID);
+
   return (
     <div>
       <button onClick={() => signIn('google')}>Sign in </button>
