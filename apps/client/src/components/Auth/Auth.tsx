5f2de172e00958170f70d9edecdc2e2a33dc389e
Amardeep Ranjan <amardeepranjan911@gmail.com>
2023-07-11 22:48:33 +0530

    added types interface for user

--- apps/client/src/components/Auth/Auth.tsx
+++ apps/client/src/components/Auth/Auth.tsx
@@ -1,5 +1,7 @@
+import { useMutation } from '@apollo/client';
 import { Button, Center, Image, Input, Stack, Text } from '@chakra-ui/react';
-
+import userOperations from '@src/graphql/operations/user';
+import { CreateUsernameData, CreateUsernameVariables } from '@src/util/types';
 import { Session } from 'next-auth';
 import { signIn } from 'next-auth/react';
 import * as React from 'react';
@@ -16,9 +18,24 @@ const Auth: React.FunctionComponent<IAuthProps> = ({
 }) => {
   const [username, setUsername] = useState('');
 
+  const [createUsername, { data, loading, error }] = useMutation<
+    CreateUsernameData,
+    CreateUsernameVariables
+  >(userOperations.Mutations.createUsername);
+
+  console.log(data);
+  console.log(loading);
+  console.log(error);
+
   const onsubmit = async () => {
+    if (!username) return;
     try {
       //create a mutation to send username to the GraphQL API
+      await createUsername({
+        variables: {
+          username,
+        },
+      });
     } catch (error) {
       console.log('error creating username');
       console.log(error);
