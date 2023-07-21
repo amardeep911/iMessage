ade639dcd24d55616a2db9245c6b3d7cedbbec98
Amardeep Ranjan <amardeepranjan911@gmail.com>
2023-07-21 23:24:59 +0530

    added feed wrapper initial setup

--- apps/client/src/components/Chat/Feed/FeedWrapper.tsx
+++ apps/client/src/components/Chat/Feed/FeedWrapper.tsx
@@ -1,9 +1,27 @@
+import { Flex } from '@chakra-ui/react';
 import { Session } from 'next-auth';
+import { useRouter } from 'next/router';
+import React from 'react';
 
 interface FeedWrapperProps {
   session: Session;
 }
 
 export const FeedWrapper: React.FC<FeedWrapperProps> = ({ session }) => {
-  return <div>FeedWrapper</div>;
+  const router = useRouter();
+
+  const { conversationId } = router.query;
+  return (
+    <Flex
+      display={{ base: conversationId ? 'flex' : 'none', md: 'flex' }}
+      width={{ base: '100%', md: '100%' }}
+      direction="column"
+    >
+      {conversationId ? (
+        <Flex>{conversationId}</Flex>
+      ) : (
+        <div>No conversation yet</div>
+      )}
+    </Flex>
+  );
 };
