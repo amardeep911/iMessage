96d9f4371f255058cbf1618294cc8b23397d8809
Amardeep Ranjan <amardeepranjan911@gmail.com>
2023-08-07 20:48:49 +0530

    added message typeDefs

--- apps/server/src/graphql/typeDefs/messages.ts
+++ apps/server/src/graphql/typeDefs/messages.ts
@@ -7,6 +7,17 @@ const typeDefs = gql`
     body: String
     createdAt: Date
   }
+  type Mutation {
+    sendMessage(
+      id: String
+      senderId: String
+      conversationId: String
+      body: String
+    ): Boolean
+  }
+  type Subscription {
+    messageSent(conversationId: String): Message
+  }
 `;
 
 export default typeDefs;
